cc3d27b2270e07111b6af2739473e02877df5621
Simo Tumelius <simo.tumelius@gmail.com>
2024-02-28 08:54:59 +0200

    Add `generate_surrogate_key` macro (#1)
    * Add generate_surrogate_key macro
    
    * Add Macros section to README

--- index.js
+++ index.js
@@ -13,6 +13,17 @@ function deduplicate(relation, partition_by, order_by) {
     `;
 }
 
-module.exports = { deduplicate };
+function generate_surrogate_key(fields, default_null_value='_qbi_dataform_utils_surrogate_key_null') {
+    var sql = ``;
+    for (let i = 0; i < fields.length; i++) {
+        sql = sql + `coalesce(cast(${fields[i]} as string), '${default_null_value}')`;
+        if (i < fields.length - 1) {
+            sql = sql + ` || '-' || `;
+        }
+    }
+    return `to_hex(md5(${sql}))`;
+}
+
+module.exports = { deduplicate, generate_surrogate_key };
 
 // see https://cloud.google.com/dataform/docs/create-package
